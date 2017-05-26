96b2543887e69fc5c7819c3de9853ce1d3156ee6
healworld1 <healworld.1@gmail.com>
2017-05-26 05:46:12 -0400

    Update mongo1.py

--- mongo1.py
+++ mongo1.py
@@ -27,11 +27,11 @@ def webhook():
     req = request.get_json(silent=True, force=True)
 
     print("Request:")
-    print(json.dumps(req, indent=4))
+    print(json.dumps(req, indent=2))
 
     res = processRequest(req)
 
-    res = json.dumps(res, indent=4)
+    res = json.dumps(res, indent=2)
     # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
